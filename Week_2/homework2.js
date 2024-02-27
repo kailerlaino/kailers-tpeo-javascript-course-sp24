dfbd4fbbfbddc4df5be9ea1a4f5f67547dd9b9ef
kailerlaino <144497906+kailerlaino@users.noreply.github.com>
2024-02-26 22:49:56 -0600

    modifying homework 2

---
+++ Week_2/homework2.js
@@ -0,0 +1,17 @@
+// Kailer Laino
+
+function sumOfSquareAreas(a, b) {
+    return (a * a) + (b * b);
+}
+
+console.log(sumOfSquareAreas(3, 4));
+console.log(sumOfSquareAreas(5, 6));
+console.log(sumOfSquareAreas(7, 8));
+
+function gradeCalculator(homeworkAvg, quizAvg, examAvg) {
+    return (homeworkAvg * 0.1) + (quizAvg * 0.15) + (examAvg * 0.75);
+}
+
+console.log(gradeCalculator(90, 80, 70));
+console.log(gradeCalculator(85, 75, 65));
+console.log(gradeCalculator(80, 70, 60));
